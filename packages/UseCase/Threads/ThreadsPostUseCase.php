4ab68f5c1d696673fe96ab83294e35c8ae143f1d
yukiarai-excite <yuki.arai@excite.jp>
2021-01-15 17:15:30 +0900

    ThreadPostUseCase作成

---
+++ packages/UseCase/Threads/ThreadsPostUseCase.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bbs\UseCase\Threads;
+
+use Bbs\Domain\Thread;
+
+class ThreadsPostUseCase implements ThreadsPostUseCaseInterface
+{
+    public function handle(string $title, string $body): void
+    {
+        // TODO: Implement handle() method.
+    }
+
+}
