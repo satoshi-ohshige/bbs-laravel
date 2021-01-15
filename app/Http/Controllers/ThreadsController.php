ae2b69ad51d2f8e8ae6b57021a6f9779aed9f62c
yukiarai-excite <yuki.arai@excite.jp>
2021-01-15 17:16:22 +0900

    [create] Threadコントローラーの作成

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Bbs\UseCase\Threads\ThreadsPostUseCaseInterface;
+use Illuminate\Http\Request;
+
 class ThreadsController extends Controller
 {
     public function create()
     {
         return view('threads.create');
+    }
+
+    public function post(Request $request, ThreadsPostUseCaseInterface $threadsPostUseCase)
+    {
+        $threadsPostUseCase->handle('sampleTitle', 'sampleBody');
     }
 }
