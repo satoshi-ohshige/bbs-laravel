cc19f257b3cc85328dc0857600f4d07645f3d95b
yukiarai-excite <yuki.arai@excite.jp>
2021-01-15 17:14:38 +0900

    [create] 投稿のルーティング作成

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TopController::class, 'index']);
 Route::get('/threads/create', [ThreadsController::class, 'create']);
+Route::post('/threads/create', [ThreadsController::class, 'post']);
+
