8fc5ef51dc100474acca4a30ec765b1fd9385023
keijiro-okamura <keijiro.okamura@excite.jp>
2021-02-05 16:49:15 +0900

    [add]ThreadをPOSTでstorage/app/persistenceに保存するように追加

--- packages/Repository/ThreadRepository.php
+++ packages/Repository/ThreadRepository.php
@@ -3,18 +3,18 @@
 namespace Bbs\Repository;
 
 use Bbs\Domain\Thread;
+use Illuminate\Support\Facades\Storage;
 
 class ThreadRepository implements ThreadRepositoryInterface
 {
     public function save(Thread $thread): void
     {
-
+        Storage::put('persistence/threads.txt', $thread->getBody());
     }
 
     public function get(int $id): Thread
     {
 
-        return new Thread($id, 'sampleTitle', 'sampleBody', new \DateTime);
-
+        return new Thread($id, 'sampleTitle', 'sampleBody', new \DateTime());
     }
 }
--- packages/UseCase/Threads/ThreadsPostUseCase.php
+++ packages/UseCase/Threads/ThreadsPostUseCase.php
@@ -3,12 +3,18 @@
 namespace Bbs\UseCase\Threads;
 
 use Bbs\Domain\Thread;
+use Bbs\Repository\ThreadRepositoryInterface;
 
 class ThreadsPostUseCase implements ThreadsPostUseCaseInterface
 {
+    public function __construct(private ThreadRepositoryInterface $threadRepository)
+    {
+    }
+
     public function handle(string $title, string $body): void
     {
         $thread = Thread::factory($title, $body, new \DateTimeImmutable());
-    }
 
+        $this->threadRepository->save($thread);
+    }
 }
