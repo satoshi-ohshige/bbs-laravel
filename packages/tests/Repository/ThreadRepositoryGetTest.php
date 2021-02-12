8bd686551ba4c093fcef601aecfb2f334ba486a5
motoki-ito <motoki.ito@excite.jp>
2021-02-12 17:15:58 +0900

    [mod] test実行前と実行後にお掃除する

--- packages/tests/Repository/ThreadRepositoryGetTest.php
+++ packages/tests/Repository/ThreadRepositoryGetTest.php
@@ -9,21 +9,33 @@ use Illuminate\Support\Facades\Storage;
 
 class ThreadRepositoryGetTest extends TestCase
 {
+    private int $targetId = 1;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Storage::delete("persistence/thread_{$this->targetId}.txt");
+    }
+
     public function test()
     {
         // 1. 準備
-        $thread = new Thread(1,'test_title','test_body', new \DateTimeImmutable());
-        Storage::put("persistence/thread_{$thread->getId()}.txt", serialize($thread));
+        $thread = new Thread($this->targetId, 'test_title', 'test_body', new \DateTimeImmutable());
+        Storage::put("persistence/thread_{$this->targetId}.txt", serialize($thread));
 
         // 2. テスト対象を実行
         $threadRepository = new ThreadRepository();
-        $response = $threadRepository->get($thread->getId());
+        $response = $threadRepository->get($this->targetId);
 
         // 3. 検証
         $this->assertEquals($thread, $response);
+    }
 
-        // 4. お片付け
-        
-
+    public function tearDown(): void
+    {
+        // parent::tearDown()内の処理によってfacadesの呼び出しができなくなるので先に実行しておく
+        Storage::delete("persistence/thread_{$this->targetId}.txt");
+        parent::tearDown();
     }
 }
