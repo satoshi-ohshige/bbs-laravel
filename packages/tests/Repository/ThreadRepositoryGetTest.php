5dcf5e673a0faf4a3611db4c58ca53191e9b25dd
takumi-ito-excite <takumi.ito@excite.jp>
2021-02-19 16:16:50 +0900

    [modify] テストをモックを使うように修正

--- packages/tests/Repository/ThreadRepositoryGetTest.php
+++ packages/tests/Repository/ThreadRepositoryGetTest.php
@@ -9,33 +9,21 @@ use Illuminate\Support\Facades\Storage;
 
 class ThreadRepositoryGetTest extends TestCase
 {
-    private int $targetId = 1;
-
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        Storage::delete("persistence/thread_{$this->targetId}.txt");
-    }
-
     public function testGetSuccess()
     {
         // 1. 準備
-        $thread = new Thread($this->targetId, 'test_title', 'test_body', new \DateTimeImmutable());
-        Storage::put("persistence/thread_{$this->targetId}.txt", serialize($thread));
+        $targetId = 1;
+        $thread = new Thread($targetId, 'test_title', 'test_body', new \DateTimeImmutable());
+        Storage::shouldReceive('get')
+            ->once()
+            ->with("persistence/thread_{$targetId}.txt")
+            ->andReturn(serialize($thread));
 
         // 2. テスト対象を実行
         $threadRepository = new ThreadRepository();
-        $response = $threadRepository->get($this->targetId);
+        $response = $threadRepository->get($targetId);
 
         // 3. 検証
         $this->assertEquals($thread, $response);
-    }
-
-    public function tearDown(): void
-    {
-        // parent::tearDown()内の処理によってfacadesの呼び出しができなくなるので先に実行しておく
-        Storage::delete("persistence/thread_{$this->targetId}.txt");
-        parent::tearDown();
     }
 }
